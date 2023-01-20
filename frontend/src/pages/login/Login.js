c0c0defc761ee6f37188cc29906f19ccdc8ad007
liezlstander <liezlstander101@gmail.com>
2023-01-20 16:52:39 +0200

    change look of login

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,99 +1,125 @@
-import React from 'react'
-import { login, checkAuthenticated, load_user } from '../../store/actions/auth'
-import { Navigate } from 'react-router-dom'
-import {useLocation} from 'react-router'
-import { useDispatch, useSelector,  } from 'react-redux'
-import { useState, useEffect } from 'react'
-import { Button } from 'flowbite-react'
+import React from "react";
+import { login, checkAuthenticated, load_user } from "../../store/actions/auth";
+import { Navigate } from "react-router-dom";
+import { useLocation } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { Button } from "flowbite-react";
+import Logo from "../../assets/icons/watermelon.png"
 
 const Login = () => {
+  const dispatch = useDispatch();
 
-	const dispatch = useDispatch()
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const loginFailed = useSelector((state) => state.auth.loginFailed);
+  const redirect = useLocation().state?.redirect;
 
-	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
-	const loginFailed = useSelector((state) => state.auth.loginFailed)
-	const redirect = useLocation().state?.redirect
+  const [error, setError] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-	const [error, setError] = useState(false)
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-	})
+  const { email, password } = formData;
 
-	const { email, password } = formData
+  const onChange = (e) => {
+    setError(false);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(e.target.email);
+  };
 
-	const onChange = (e) => {
-		setError(false)
-		setFormData({ ...formData, [e.target.name]: e.target.value })
-		console.log(e.target.email)
-	}
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
 
-	const onSubmit = (e) => {
-		e.preventDefault()
-		dispatch(login(email, password))
-	}
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      dispatch(login(email, password));
+    }
+  };
 
-	const handleKeyDown = (e) => {
-		if (e.key === 'Enter') {
-			dispatch(login(email, password))
-		}
-	}
+  useEffect(() => {
+    dispatch(checkAuthenticated());
+    dispatch(load_user());
+  });
 
-	useEffect(() => {
-		dispatch(checkAuthenticated())
-		dispatch(load_user())
-	}, )
+  useEffect(() => {
+    if (loginFailed) {
+      setError(true);
+      setFormData({
+        email: "",
+        password: "",
+      });
+    }
+  }, [loginFailed]);
 
-	useEffect(() => {
-		if (loginFailed) {
-			setError(true)
-			setFormData({
-				email: '',
-				password: '',
-			})
-		}
-	}, [loginFailed])
+  if (isAuthenticated === true) {
+    return <Navigate to={redirect ? redirect : "/"} />;
+  }
 
-	if (isAuthenticated === true) {
-		return <Navigate to={redirect ? redirect : '/'} />
-	}
-    
-	return (
-		<div className="loginContainer shadow-card p-6 rounded flex flex-col gap-4 bg-white h-full">
-			<h2 className="text-center font-semibold text-2xl">Login</h2>
-			<div className="loginInputs flex flex-col gap-4">
-				<div className="flex flex-col gap-1">
-					<label className="text-sm font-semibold" htmlFor="email">Email</label>
-					<input 
-						className={`bg-transparent border border-${error ? 'red-600' : 'gray-400'} rounded-sm p-3 text-sm`}
-						type="text" 
-						name="email"
-						id="email"
-						onChange={onChange}
-						// onKeyDown={handleKeyDown}
-						// value={email}
-					/>
-				</div>
-				<div className="flex flex-col gap-1">
-					<label className="text-sm font-semibold" htmlFor="password">Password</label>
-					<input 
-						className={`bg-transparent border border-${error ? 'red-600' : 'gray-400'} rounded-sm p-3 text-sm`}
-						type="password" 
-						name="password"
-						id="password"
-						onChange={onChange}
-						onKeyDown={handleKeyDown}
-						// value={password}
-					/>
-				</div>
-				<Button onClick={onSubmit}>Login</Button>
-				{error && (
-					<div className="text-red-600">Login failed</div>
-				)}
-			</div>
-            
-		</div>
-	)
-}
+  return (
+    <>
+      <div className="flex bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 h-screen">
+        <div className="m-auto">
+          {/* <div className="flex justify-center">
+            <img className="animate-bounce h-80" src={Logo} alt="" />
+          </div> */}
 
-export default Login
+          <div className="container flex flex-nowrap gap-12 justify-around">
+            <div className="max-w-[80%] flex flex-col justify-center">
+              <div className="text-center text-5xl font-extrabold">
+                <div className="bg-clip-text text-transparent bg-white pb-4">
+                  Welcome to Expensify
+                </div>
+              </div>
+              <p className="text-center bg-clip-text text-transparent bg-white">
+                Your all in one expense tracker. Track your expenses, create
+                budgets, and manage your money.
+              </p>
+            </div>
+			<div className="loginInputs flex flex-col gap-4 min-w-[50%]">
+			<div className="flex flex-col gap-1">
+            <label className="text-sm font-semibold text-white" htmlFor="email">
+              Email
+            </label>
+            <input
+              className={`bg-transparent border border-${
+                error ? "red-600" : "white"
+              } rounded-lg p-3 text-sm mb-4`}
+              type="text"
+              name="email"
+              id="email"
+              onChange={onChange}
+              // onKeyDown={handleKeyDown}
+              // value={email}
+            />
+			          <div className="flex flex-col gap-1">
+            <label className="text-sm font-semibold text-white" htmlFor="password">
+              Password
+            </label>
+            <input
+              className={`bg-transparent border border-${
+                error ? "red-600" : "white"
+              } rounded-lg p-3 text-sm mb-4`}
+              type="password"
+              name="password"
+              id="password"
+              onChange={onChange}
+              onKeyDown={handleKeyDown}
+              // value={password}
+            />
+			<Button className="!bg-red-400" onClick={onSubmit}>Login</Button>
+          {error && <div className="text-red-600">Login failed</div>}
+          </div>
+          </div>
+		  </div>
+          </div>
+          <div className="flex justify-center"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
